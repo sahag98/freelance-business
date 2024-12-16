a00c470bf30063c00353e9559b71a248d50c8126
Sahag Arzoumanian <arzsahag@gmail.com>
2024-12-16 12:01:57 -0800

    change dot pattern color

--- components/ui/dot-pattern.tsx
+++ components/ui/dot-pattern.tsx
@@ -30,8 +30,8 @@ export function DotPattern({
     <svg
       aria-hidden="true"
       className={cn(
-        "pointer-events-none absolute inset-0 h-full w-full fill-neutral-400/80",
-        className,
+        "pointer-events-none absolute inset-0 h-full w-full fill-neutral-400/60",
+        className
       )}
       {...props}
     >
