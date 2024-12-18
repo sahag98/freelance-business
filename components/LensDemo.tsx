7554427e394f9e4639d9b20b87d2bd6af3c99231
Sahag Arzoumanian <arzsahag@gmail.com>
2024-12-18 11:56:25 -0800

    Remove unused imports in portfolio item

--- components/LensDemo.tsx
+++ components/LensDemo.tsx
@@ -1,19 +1,9 @@
-"use client";
-import { useState } from "react";
-import { Lens } from "./ui/lens";
 import Image from "next/image";
-import { motion } from "framer-motion";
-
-import { Globe } from "lucide-react";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 
-export function LensDemo({ item, key }: any) {
-  const [hovering, setHovering] = useState(false);
-
-  console.log("key: ", item.id);
-
+export function LensDemo({ item }: any) {
   return (
     <div className="portfolio-card">
       <div className="w-full flex relative border rounded-xl overflow-hidden bg-gray-50 dark:bg-secondary p-4">
@@ -31,12 +21,7 @@ export function LensDemo({ item, key }: any) {
             className="rounded-2xl"
           />
 
-          <motion.div
-            animate={{
-              filter: hovering ? "blur(2px)" : "blur(0px)",
-            }}
-            className="py-4 h-full flex flex-col justify-center gap-5  md:w-1/2 w-full relative z-20"
-          >
+          <div className="py-4 h-full flex flex-col justify-center gap-5  md:w-1/2 w-full relative z-20">
             <div className="flex items-center justify-between">
               <h2 className=" text-2xl text-left font-bold">{item.title}</h2>
             </div>
@@ -52,7 +37,7 @@ export function LensDemo({ item, key }: any) {
                 Visit Website
               </Button>
             </Link>
-          </motion.div>
+          </div>
         </div>
       </div>
     </div>
