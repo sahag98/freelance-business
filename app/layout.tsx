6357bc31733d6d1213792245f13036d2895e6e17
Sahag Arzoumanian <arzsahag@gmail.com>
2025-01-15 14:13:38 -0800

    use next/image and remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter, Montserrat, Orbitron, Raleway } from "next/font/google";
+import { Montserrat, Orbitron } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const orbitron = Orbitron({
